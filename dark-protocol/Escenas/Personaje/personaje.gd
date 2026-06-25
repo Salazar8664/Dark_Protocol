070ec7cb57d900eaafbacc3c01c1fc909f61b4a9
Salazar8664 <camiloandressalazarj@gmail.com>
2026-06-24 19:36:23 -0500

    configuracion inicial del proyecto y escenas base

---
+++ dark-protocol/Escenas/Personaje/personaje.gd
@@ -0,0 +1,57 @@
+extends CharacterBody2D
+@export var animacion : AnimatedSprite2D
+@export var area_2d : Area2D
+@export var Material_Personaje_rojo: ShaderMaterial
+signal PersonajeMuerto
+var _velocidad: float = 100.0 
+var _velocidad_salto: float = -300
+var _Muerto: bool
+func _ready():
+	add_to_group("Personajes")
+	area_2d.body_entered.connect(_on_area_2d_body_entered)
+	
+	
+func _physics_process(delta):
+	if _Muerto:
+		return
+	#gravedad
+	velocity += get_gravity() * delta
+	#salto
+	if Input.is_action_just_pressed("saltar") && is_on_floor():
+		velocity.y = _velocidad_salto
+	#velosidad
+	if Input.is_action_pressed("dereha"):
+		velocity.x = _velocidad
+		animacion.flip_h = false
+	elif Input.is_action_pressed("izquierda"):
+		velocity.x = -_velocidad
+		animacion.flip_h = true
+	else:
+		velocity.x = 0
+	move_and_slide()
+	#animacion
+	if !is_on_floor():
+		if velocity.y < 0:
+			animacion.play("saltar")
+		else:
+			animacion.play("caer")
+	elif velocity.x !=0:
+		animacion.play("Correr")
+	else:
+		animacion.play("idle")
+
+
+func _on_area_2d_body_entered(_body: Node2D) -> void:
+	animacion.material =  Material_Personaje_rojo
+	_Muerto = true
+	animacion.stop()
+	var timer: Timer = Timer.new()
+	add_child(timer)
+	timer.start(0.5)
+	await timer.timeout
+	PersonajeMuerto.emit()
+	
+	
+	
+	
+	
